4872015222856e66da5674816e267a600d91e707
Seo-Yurim <yurim2204@naver.com>
2024-09-10 14:44:21 +0900

    Feat: app.js 구현

---
+++ app.js
@@ -0,0 +1,113 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import Product from './models/Product.js';
+import cors from 'cors';
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log('Connected to DB'));
+
+const app = express();
+
+const corsOptions = {
+  origin: ['http://127.0.0.1:4000', 'https://netlify.com']
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+function asyncHandler(handler) {
+  // 함수를 인수로 받아서 함수를 반환
+  const asyncReqHandler = async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (err) {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+      } else if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Cannot find given id.' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    }
+  };
+  return asyncReqHandler;
+}
+
+// 상품 등록
+app.post(
+  '/products',
+  asyncHandler(async (req, res) => {
+    const newProduct = await Product.create(req.body);
+    res.status(201).send(newProduct);
+  })
+);
+
+// 상품 상세 조회
+app.get(
+  '/products/:id',
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id, { updatedAt: 0 });
+
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: '해당 상품을 찾을 수 없습니다.' });
+    }
+  })
+);
+
+// 상품 수정
+app.patch(
+  '/products/:id',
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+
+    if (product) {
+      Object.keys(req.body).forEach((key) => {
+        product[key] = req.body[key];
+      });
+      await product.save();
+      res.send(product);
+    } else {
+      res.status(404).send({ message: '해당 상품을 찾을 수 없습니다.' });
+    }
+  })
+);
+
+// 상품 삭제
+app.delete(
+  '/products/:id',
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (product) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send({ message: '해당 상품을 찾을 수 없습니다.' });
+    }
+  })
+);
+
+// 상품 목록 조회
+app.get(
+  '/products',
+  asyncHandler(async (req, res) => {
+    const sort = req.query.sort;
+    const sortOption = { createdAt: sort === 'recent' ? 'desc' : 'asc' };
+
+    const count = 3;
+
+    const products = await Product.find({}, { name: 1, price: 1, createdAt: 1 })
+      .sort(sortOption)
+      .limit(count);
+    res.send(products);
+  })
+);
+
+app.listen(process.env.PORT, () => console.log('server on'));
