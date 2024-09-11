7c5e8c504d60083d72b9b55481204d292ac4401e
bangsinchur <sinchurbang123@gmail.com>
2024-09-11 14:26:00 +0900

    배포하기전 commit

--- PANDA-API/data/seed.js
+++ PANDA-API/data/seed.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-import data from './mock.js';
+import data from "./mock.js";
 import Item from "../models/Product.js";
-import { DATABASE_URL } from "../env.js";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect(DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL);
 
 await Item.deleteMany({});
 await Item.insertMany(data);
