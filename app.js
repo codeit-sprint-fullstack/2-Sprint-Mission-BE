59b0d7f690063d6f7677460286fbe243d5b3d69a
unknown <d653@naver.com>
2024-09-11 18:39:36 +0900

    Feat: getting, creating, patching, deleting  api function
    fix app.js

--- app.js
+++ app.js
@@ -26,4 +26,79 @@ function asyncHandler(handler) {
   return asyncHandler;
 }
 
+app.post(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const newProduct = await Product.create(req.body);
+    res.status(201).send(newProduct);
+  })
+);
+
+app.get(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Cannot find given id." });
+    }
+  })
+);
+
+app.patch(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (product) {
+      Object.keys(req.body).forEach((key) => {
+        product[key] = req.body[key];
+      });
+      await product.save();
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Cannot find given id." });
+    }
+  })
+);
+
+app.delete(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (product) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send({ message: "Cannot find given id." });
+    }
+  })
+);
+
+app.get(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, sort = "recent", search = "" } = req.query;
+    const skip = (page - 1) * limit;
+    const sortOption = { createdAt: sort === "recent" ? "desc" : "asc" };
+
+    const searchQuery = {
+      $or: [
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ],
+    };
+
+    const products = await Product.find(searchQuery)
+      .sort(sortOption)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(products);
+  })
+);
+
 app.listen(3000, () => console.log("server on"));
