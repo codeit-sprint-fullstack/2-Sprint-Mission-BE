6ca558e3e34c3b3c823ccc996651ebf4e7b1fc17
unknown <d653@naver.com>
2024-09-11 18:32:30 +0900

    Fix: app.js

--- app.js
+++ app.js
@@ -9,4 +9,21 @@ const app = express();
 
 app.use(express.json());
 
+function asyncHandler(handler) {
+  const asyncHandler = async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e.name === "ValidationError") {
+        res.status(400).send({ message: e.message });
+      } else if (e.name === "CastError") {
+        res.stats(404).send({ message: "Cannot find given id." });
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+  return asyncHandler;
+}
+
 app.listen(3000, () => console.log("server on"));
