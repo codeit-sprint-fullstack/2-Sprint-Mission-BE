de3aa5aeed8e0d6e72db3ffe595e47a9e62a68d5
BuffSo <buffso@naver.com>
2024-09-12 10:12:02 +0900

    fix: remove trailing slash from CORS options for Netlify URL

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.DATABASE_URL).then(() => console.log('connected to 
 const app = express();  
 
 const corsOptions = {
-  origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://buffso-pandamarket.netlify.app/']
+  origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://buffso-pandamarket.netlify.app']
 }
 app.use(cors(corsOptions));
 app.use(express.json());  // JSON 요청 파싱 미들웨어
