897c57301854ed843be1ae85afcfe11e7d5e51ca
shinji530 <shinji53059@gmail.com>
2024-09-13 10:14:11 +0900

    Feat: DB 연동

---
+++ app.js
@@ -0,0 +1,97 @@
+import express from "express";
+import mongoose from "mongoose";
+import Product from "./models/Product.js";
+import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL).then(() => console.log("Connected to DB"));
+
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:3000", 'https://panda-market-react.netlify.app'],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e.name === "ValildationError") {
+        res.status(400).send({ message: e.message });
+      } else if (e.name === "CastError") {
+        res.status(404).send({ message: "Cannot find given id." });
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+}
+
+app.get("/products", 
+  asyncHandler(async (req, res) => {
+    const sort = req.query.sort;
+    const count = Number(req.query.count) || 0;
+
+    const sortOptions = { createdAt: sort === "recent" ? "asc" : "desc" };
+    const products = await Product.find().sort(sortOptions).limit(count);
+
+    res.send(products);
+  })
+);
+
+app.get("/products/:id", 
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Cannot find given id." });
+    }
+  })
+);
+
+app.post("/products", 
+  asyncHandler(async (req, res) => {
+    const product = await Product.create(req.body);
+
+    res.status(201).send(product);
+  })
+);
+
+app.patch("/products/:id", 
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+
+    if(product) {
+      Object.keys(req.body).forEach((key) => {
+        product[key] = req.body[key];
+      });
+      await product.save();
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Cannot find given id." });
+    }
+  })
+);
+
+app.delete("/products/:id", 
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await product.findByIdAndDelete(id);
+
+    if (product) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send({ message: "Cannot find given id." });
+    }
+  })
+);
+
+app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
---
+++ data/mock.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    name: "쿠쿠 밥솥",
+    description: "쿠쿠하세요~~",
+    price: 310000,
+    tags: ["쿠쿠쿠쿠쿸"],
+    createdAt: "2024-09-01T15:56:53.858Z",
+    updatedAt: "2024-09-02T01:15:56.841Z",
+  },
+  {
+    name: "토스터",
+    description: "토스트를 만들기 위한 토스터~",
+    price: 19000,
+    tags: ["토스터"],
+    createdAt: "2024-08-27T12:26:55.629Z",
+    updatedAt: "2024-08-30T10:42:33.978Z",
+  },
+  {
+    name: "상품 이름",
+    description: "string",
+    price: 0,
+    tags: [],
+    createdAt: "2024-08-25T17:41:10.901Z",
+    updatedAt: "2024-09-04T08:20:57.019Z",
+  },
+  {
+    name: "화사한 분위기의 수련",
+    description: "무료 이미지 꽃, 수련, water lily",
+    price: 12000,
+    tags: ["꽃", "수련", "water lily"],
+    createdAt: "2024-08-20T02:26:50.914Z",
+    updatedAt: "2024-08-21T02:06:50.311Z",
+  },
+  {
+    name: "밝은 분위기의 벽지와 전등",
+    description: "무료 이미지 전등, 벽지, 인테리어",
+    price: 36000,
+    tags: ["전등", "벽지", "인테리어"],
+    createdAt: "2024-08-20T02:12:42.228Z",
+    updatedAt: "2024-08-21T02:06:47.586Z",
+  },
+];
+
+export default data;
