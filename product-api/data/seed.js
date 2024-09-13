8f089701832e176ff2a4171cfd44a01536541d8b
kipid <kipacti@gmail.com>
2024-09-13 20:50:54 +0900

    Update seed.js

--- product-api/data/seed.js
+++ product-api/data/seed.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import faker from 'faker';
 import data from './mock.js';
 import Product from '../models/Product.js';
 import * as dotenv from 'dotenv';
@@ -7,6 +8,17 @@ dotenv.config();
 mongoose.connect(process.env.DATABASE_URL); // Open
 
 await Product.deleteMany({});
-await Product.insertMany(data);
 
-mongoose.connection.close(); // Close
+async function generateMockData() {
+    const products = data;
+    for (let product of products) {
+        product.price = faker.commerce.price(1, 1_000_000);
+				product.favoriteCount = faker.datatype.number({ min: 0, max: 10000 });
+    }
+
+    await Product.insertMany(products);
+    console.log('Mock data generated successfully!');
+    await mongoose.disconnect();
+}
+
+generateMockData().catch(console.error);
