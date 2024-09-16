6e7dd05f6b5a21f2960c8c9df3a685c0f3147185
Seo-Yurim <yurim2204@naver.com>
2024-09-16 14:00:56 +0900

    Refactor: Netlify 주소 변경

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 const corsOptions = {
   origin: [
     'http://localhost:3000',
-    'https://welcome-to-pandamarket.netlify.app/'
+    'https://welcome-to-pandamarket.netlify.app/items'
   ]
 };
 
