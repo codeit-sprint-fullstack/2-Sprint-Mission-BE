0d0b5a81574f55975921960e46b29b9c57c68a9e
TaeyeonKim <gggm0117@naver.com>
2024-09-24 18:44:02 +0900

    1

--- prisma/seed.js
+++ prisma/seed.js
@@ -8,26 +8,36 @@ async function main() {
   await prisma.article.deleteMany();
   await prisma.productComment.deleteMany();
   await prisma.articleComment.deleteMany();
+  console.log("createUser");
   await prisma.user.createMany({
     data: mockData.user,
     skipDuplicates: true
   });
+  console.log("createUser END");
+  console.log("createProduct start");
   await prisma.product.createMany({
     data: mockData.product,
     skipDuplicates: true
   });
+  console.log("createProduct END");
+  console.log("crateAriticle start");
   await prisma.article.createMany({
     data: mockData.article,
     skipDuplicates: true
   });
+  console.log("crateAriticle end");
+  console.log("productComment start");
   await prisma.productComment.createMany({
     data: mockData.productComment,
     skipDuplicates: true
   });
+  console.log("productComment end");
+  console.log("articleComment start");
   await prisma.articleComment.createMany({
     data: mockData.articleComment,
     skipDuplicates: true
   });
+  console.log("articleComment end");
 }
 main()
   .then(async () => {
