e0f260de198001cdbd4742c1f7f0dd0c5e097ca9
TaeyeonKim <gggm0117@naver.com>
2024-09-24 16:20:39 +0900

    Implement: create mockData

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,25 +1,33 @@
 import { PrismaClient } from "@prisma/client";
-import { mockProductData, mockUserData, mockArticleData } from "./mock.js";
+import mockData from "./mock.js";
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.user.deleteMany();
   await prisma.product.deleteMany();
-  await prisma.product.createMany({
-    data: mockProductData,
+  await prisma.article.deleteMany();
+  await prisma.productComment.deleteMany();
+  await prisma.articleComment.deleteMany();
+  await prisma.user.createMany({
+    data: mockData.user,
     skipDuplicates: true
   });
-  // await prisma.user.deleteMany();
-  // await prisma.user.createMany({
-  //   data: mockUserData,
-  //   skipDuplicates: true
-  // });
-  // await prisma.article.deleteMany();
-  // const articleSeed = mockArticleData.map(async (article) => {
-  //   await prisma.article.create({
-  //     data: article
-  //   });
-  // });
-  // await Promise.all(articleSeed);
+  await prisma.product.createMany({
+    data: mockData.product,
+    skipDuplicates: true
+  });
+  await prisma.article.createMany({
+    data: mockData.article,
+    skipDuplicates: true
+  });
+  await prisma.productComment.createMany({
+    data: mockData.productComment,
+    skipDuplicates: true
+  });
+  await prisma.articleComment.createMany({
+    data: mockData.articleComment,
+    skipDuplicates: true
+  });
 }
 main()
   .then(async () => {
