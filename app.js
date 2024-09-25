98a30228ee1c9438614c08786719fa4084aec261
Hailey Kim <hailey@Haileys-Laptop.local>
2024-09-25 17:30:39 +0900

    fix: change search api endpoint

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import express from "express";
-import cors from 'cors';
-import * as dotenv from 'dotenv';
-import Product from './Models/Product.js';
+import cors from "cors";
+import * as dotenv from "dotenv";
+import Product from "./Models/Product.js";
 dotenv.config();
 
-mongoose.connect(process.env.DATABASE_URL).then(() => console.log("Connected to DB"));
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to DB"));
 
 const app = express();
 // const corsOptions = {
@@ -15,90 +17,102 @@ app.use(cors());
 app.use(express.json());
 
 function asyncHandler(handler) {
-  const newHandler = async function(req, res) {
+  const newHandler = async function (req, res) {
     try {
       await handler(req, res);
     } catch (e) {
-      if (e.name === 'ValidationError') {
-        res.status(400).send({message: e.message});
-      } else if (e.name === 'CastError') {
-        res.status(404).send({message: '해당 데이터를 찾을 수 없습니다!'});
+      if (e.name === "ValidationError") {
+        res.status(400).send({ message: e.message });
+      } else if (e.name === "CastError") {
+        res.status(404).send({ message: "해당 데이터를 찾을 수 없습니다!" });
       } else {
-        res.status(500).send({message: e.message});
+        res.status(500).send({ message: e.message });
       }
     }
-  }
+  };
   return newHandler;
 }
 
-app.get("/products", asyncHandler(async (req, res) => {
-  const sortOption = req.query.orderBy || {createdAt: 'desc'};
-  const page = Number(req.query.page) || 1;
-  const pageSize = Number(req.query.pageSize) || 10;
-  const offset = (page - 1) * pageSize;
-  
-  const totalCount = await Product.countDocuments();
-  const products = await Product.find().sort(sortOption).skip(offset).limit(pageSize);
-  res.send({products, totalCount});
-}));
+app.get(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const sortOption = req.query.orderBy || { createdAt: "desc" };
+    const page = Number(req.query.page) || 1;
+    const pageSize = Number(req.query.pageSize) || 10;
+    const offset = (page - 1) * pageSize;
+    const search = req.query.search || "";
 
-app.get("/products/search", asyncHandler(async (req, res) => {
-  const { query } = req.query;
+    const totalCount = await Product.countDocuments();
+    const products = await Product.find({
+      $or: [
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ],
+    })
+      .sort(sortOption)
+      .skip(offset)
+      .limit(pageSize);
 
-  const products = await Product.find({
-    $or: [
-      { name: { $regex: query, $options: 'i' } },
-      { description: { $regex: query, $options: 'i' } }
-    ]
+    if (products.length > 0) {
+      res.send({ products, totalCount });
+    } else {
+      res.status(404).send({ message: "검색 결과가 없습니다!" });
+    }
   })
+);
 
-  if (products.length > 0) {
-    res.send(products);
-  } else {
-    res.status(404).send({ message: '검색 결과가 없습니다!' })
-  }
-}))
+app.get(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
 
-app.get("/products/:id", asyncHandler(async (req, res) => {
-  const id = req.params.id;
-  const product = await Product.findById(id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "해당 데이터를 찾을 수 없습니다!" });
+    }
+  })
+);
 
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({message: '해당 데이터를 찾을 수 없습니다!'});
-  }
-}))
+app.post(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const newProduct = await Product.create(req.body);
+    res.send(newProduct);
+  })
+);
 
-app.post("/products", asyncHandler(async (req, res) => {
-  const newProduct = await Product.create(req.body);
-  res.send(newProduct);
-}))
+app.patch(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
 
-app.patch('/products/:id', asyncHandler(async (req, res) => {
-  const id = req.params.id;
-  const product = await Product.findById(id);
+    if (product) {
+      Object.keys(req.body).forEach((key) => {
+        product[key] = req.body[key];
+      });
+      await product.save();
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "해당 데이터를 찾을 수 없습니다!" });
+    }
+  })
+);
 
-  if (product) {
-    Object.keys(req.body).forEach((key) => {
-      product[key] = req.body[key];
-    });
-    await product.save();
-    res.send(product);
-  } else {
-    res.status(404).send({message: '해당 데이터를 찾을 수 없습니다!'});
-  }
-}))
+app.delete(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
 
-app.delete("/products/:id", asyncHandler(async (req, res) => {
-  const id = req.params.id;
-  const product = await Product.findByIdAndDelete(id);
-
-  if (product) {
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(404).send({message: '해당 데이터를 찾을 수 없습니다!'})
-  }
-}))
+    if (product) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404).send({ message: "해당 데이터를 찾을 수 없습니다!" });
+    }
+  })
+);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server on"));
