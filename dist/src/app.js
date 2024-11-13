ca7bcac61497662783eda63354bf59ad83490a62
alscksdlek <164968618+alscksdlek@users.noreply.github.com>
2024-11-14 01:15:24 +0900

    remove: Delete dist directory

--- dist/src/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const appConfig_1 = require("./appConfig");
-const app = (0, appConfig_1.appConfig)();
-app.listen(3000, () => {
-    console.log("Server Started");
-});
--- dist/src/singleton/productContainerSingleton.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productContainer = void 0;
-const productContainer_1 = require("../containers/productContainer");
-exports.productContainer = new productContainer_1.ProductContainer();
