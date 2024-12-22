f2d7311a1c26f90b3f9100c1e8f25dc341b74616
alscksdlek <mseon916@naver.com>
2024-12-23 02:12:49 +0900

    Feat: winston 이용 logging

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module.js';
 import { NestFactory } from '@nestjs/core';
 import { GlobalExceptionFilter } from './common/exceptions/global-exception-filter.js';
+import { LoggingInterceptor } from './common/logger/logging.interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new GlobalExceptionFilter());
 
