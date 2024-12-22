f0241870af9e354abc7bd59360402a4f88ddd0d7
alscksdlek <mseon916@naver.com>
2024-12-23 01:08:21 +0900

    Remove: express-김민선 merge 파일 삭제

--- prisma/migrations/20241114162328_delete_optional_encrypted_password/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `encryptedPassword` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "encryptedPassword" SET NOT NULL;
--- src/types/productType.ts
+++
@@ -1,6 +0,0 @@
-export interface Product {
-  name: string;
-  description: string;
-  price: number;
-  userId: string;
-}
